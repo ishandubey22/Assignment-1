6c87c966c1b10a04d7245085e2d740d25bd23683
ishandubey22 <135635193+ishandubey22@users.noreply.github.com>
2024-09-22 18:57:50 +0530

    Update a1-4.cpp

--- a1-4.cpp
+++ a1-4.cpp
@@ -1,54 +1,57 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
-//write your code here
-
-// Function to find the median
 double findMedian(int arr1[], int size1, int arr2[], int size2) {
-    
-// Your code here
+    if (size1 > size2) return findMedian(arr2, size2, arr1, size1); // ensure size1 <= size2
 
+    int low = 0, high = size1;
+    int halfLen = (size1 + size2 + 1) / 2;
+    
+    while (low <= high) {
+        int i = (low + high) / 2;
+        int j = halfLen - i;
+
+        int maxLeftA = (i == 0) ? INT_MIN : arr1[i - 1];
+        int minRightA = (i == size1) ? INT_MAX : arr1[i];
+
+        int maxLeftB = (j == 0) ? INT_MIN : arr2[j - 1];
+        int minRightB = (j == size2) ? INT_MAX : arr2[j];
+
+        if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+            if ((size1 + size2) % 2 == 0)
+                return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+            else
+                return max(maxLeftA, maxLeftB);
+        } else if (maxLeftA > minRightB) {
+            high = i - 1;
+        } else {
+            low = i + 1;
+        }
+    }
+
+    return 0.0; // should never reach here
 }
 
-
 int main() {
-    // You can use cout statements, but ensure that you submit after commenting them out, as the autograder will otherwise mistake it for your actual output
-    // Everything else in the main function remains unchanged
-    
     int size1;
-    // cout << "Enter the size of the array1: ";
     cin >> size1;
 
     int arr[size1];
-    // cout << "Enter " << size1 << " elements:" << endl;
     for (int i = 0; i < size1; ++i) {
         cin >> arr[i];
     }
 
-    // cout << "The array elements are: ";
-    // for (int i = 0; i < size1; ++i) {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << endl;
-
     int size2;
-    // cout << "Enter the size of the array2: ";
     cin >> size2;
 
     int arr1[size2];
-    // cout << "Enter " << size2 << " elements:" << endl;
     for (int i = 0; i < size2; ++i) {
         cin >> arr1[i];
     }
-
-    // cout << "The array elements are: ";
-    // for (int i = 0; i < size2; ++i) {
-    //     cout << arr1[i] << " ";
-    // }
-    // cout << endl;
     
-    cout << "Median = " << findMedian(arr, size1, arr1, size2) << endl; // do not comment this out
+    cout << "Median = " << findMedian(arr, size1, arr1, size2) << endl;
     
     return 0;
 }
