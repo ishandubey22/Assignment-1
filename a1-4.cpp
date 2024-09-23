43489db327e63983037a994704a4447bcd7ae575
ishandubey22 <135635193+ishandubey22@users.noreply.github.com>
2024-09-23 16:54:08 +0530

    a1-4.cpp

--- a1-4.cpp
+++ a1-4.cpp
@@ -3,9 +3,28 @@ using namespace std;
 
 // Function to find the median of two sorted arrays
 double findMedian(int arr1[], int size1, int arr2[], int size2) {
+    // Handle cases where one of the arrays is empty
+    if (size1 == 0 && size2 == 0) {
+        return 0.0; // Undefined behavior; you might want to return a specific message.
+    }
+    if (size1 == 0) {
+        if (size2 % 2 == 0) {
+            return (arr2[size2 / 2] + arr2[size2 / 2 - 1]) / 2.0;
+        } else {
+            return arr2[size2 / 2];
+        }
+    }
+    if (size2 == 0) {
+        if (size1 % 2 == 0) {
+            return (arr1[size1 / 2] + arr1[size1 / 2 - 1]) / 2.0;
+        } else {
+            return arr1[size1 / 2];
+        }
+    }
+
     int totalSize = size1 + size2;
     int medianPos = (totalSize - 1) / 2;
-    
+
     int i = 0, j = 0, count = 0;
     int prev = -1, current = -1;
 
@@ -20,7 +39,7 @@ double findMedian(int arr1[], int size1, int arr2[], int size2) {
             current = arr2[j];
             j++;
         }
-        
+
         if (count == medianPos) {
             if (totalSize % 2 == 0) {
                 return (current + prev) / 2.0;
@@ -36,7 +55,7 @@ double findMedian(int arr1[], int size1, int arr2[], int size2) {
         prev = current;
         current = arr1[i];
         i++;
-        
+
         if (count == medianPos) {
             if (totalSize % 2 == 0) {
                 return (current + prev) / 2.0;
@@ -52,7 +71,7 @@ double findMedian(int arr1[], int size1, int arr2[], int size2) {
         prev = current;
         current = arr2[j];
         j++;
-        
+
         if (count == medianPos) {
             if (totalSize % 2 == 0) {
                 return (current + prev) / 2.0;
@@ -82,7 +101,7 @@ int main() {
     for (int i = 0; i < size2; ++i) {
         cin >> arr1[i];
     }
-    
+
     cout << "Median = " << findMedian(arr, size1, arr1, size2) << endl;
 
     return 0;
